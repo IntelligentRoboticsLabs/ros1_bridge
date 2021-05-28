49f1223ccc3d43590b343af3bf71c886c9455d00
jginesclavero <jonathangines@hotmail.com>
2021-05-28 20:02:52 +0200

    Fix camera_info bridge

--- src/camera_info_1_to_2.cpp
+++ src/camera_info_1_to_2.cpp
@@ -39,25 +39,43 @@ void CameraInfoCallback(boost::shared_ptr<sensor_msgs::CameraInfo> ros1_msg)
   if (pub->get_subscription_count() == 0)
     return;
 
-  auto ros2_msg = std::make_unique<sensor_msgs::CameraInfo>();
+  auto ros2_msg = std::make_unique<sensor_msgs::msg::CameraInfo>();
 
   ros2_msg->header.frame_id = ros1_msg->header.frame_id;
   ros2_msg->header.stamp = rclcpp::Time(ros1_msg->header.stamp.toNSec());
   ros2_msg->height = ros1_msg->height;
   ros2_msg->width = ros1_msg->width;
   ros2_msg->distortion_model = ros1_msg->distortion_model;
+  ros2_msg->d.resize(ros1_msg->D.size());
   for (long unsigned int i = 0; i < ros1_msg->D.size(); i++) {
-    ros2_msg->D[i] = ros1_msg->D[i];
+    ros2_msg->d[i] = ros1_msg->D[i];
   }
-  for (long unsigned int i = 0; i < ros1_msg->K.size(); i++) {
-    ros2_msg->K[i] = ros1_msg->K[i];
+  
+  std::array<double, 9> k_data, r_data;
+  std::array<double, 12> p_data;
+  int i = 0, j = 0, k = 0;
+  
+  for (auto & data : ros1_msg->K)
+  {
+    k_data[i] = data;
+    i++;
   }
-  for (long unsigned int i = 0; i < ros1_msg->R.size(); i++) {
-    ros2_msg->R[i] = ros1_msg->R[i];
+  ros2_msg->k = k_data;
+
+  for (auto & data : ros1_msg->R)
+  {
+    r_data[j] = data;
+    j++;
   }
-  for (long unsigned int i = 0; i < ros1_msg->P.size(); i++) {
-    ros2_msg->P[i] = ros1_msg->P[i];
+  ros2_msg->r = r_data;
+
+  for (auto & data : ros1_msg->P)
+  {
+    p_data[k] = data;
+    k++;
   }
+  ros2_msg->p = p_data;
+  
   ros2_msg->binning_x = ros1_msg->binning_x;
   ros2_msg->binning_y = ros1_msg->binning_y;
   ros2_msg->roi.x_offset = ros1_msg->roi.x_offset;
@@ -74,12 +92,12 @@ int main(int argc, char * argv[])
   // ROS 2 node and publisher
   rclcpp::init(argc, argv);
   auto node = rclcpp::Node::make_shared("camera_info_1_to_2");
-  pub = node->create_publisher<sensor_msgs::msg::CameraInfo>("output", 100);
+  pub = node->create_publisher<sensor_msgs::msg::CameraInfo>("output", 1);
 
   // ROS 1 node and subscriber
   ros::init(argc, argv, "camera_info_1_to_2");
   ros::NodeHandle n;
-  ros::Subscriber sub = n.subscribe("input", 100, CameraInfoCallback);
+  ros::Subscriber sub = n.subscribe("input", 1, CameraInfoCallback);
 
   ros::spin();
 
